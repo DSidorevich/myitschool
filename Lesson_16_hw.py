5a9d90fb1ef457239a244a0b5e8532ab2bc4a4e7
Dmitry_Sidorevich <rion88@yandex.ru>
2023-11-26 23:29:13 +0300

    home work is done

---
+++ Lesson_16_hw.py
@@ -0,0 +1,47 @@
+# Домашнее задание
+# Задача №1
+# Ввод с клавиатуры. Если строка введённая с клавиатуры –
+# это числа, то поделить первое на второе. Обработать
+# ошибку деления на ноль. Если второе число 0, то
+# программа запрашивает ввод чисел заново. Также если
+# были введены буквы, то обработать исключение.
+
+
+# Вариант 1
+while True:
+    try:
+        number1 = input('Input 1st number: ')
+        number2 = input('Input 2nd number: ')
+        result = int(number1) / int(number2)
+        print(number1, '/', number2, '=', result)
+    except ValueError:
+        print('Error! Incorrect number is entered')
+        continue
+    except ZeroDivisionError:
+        print('Error! Zero division')
+        continue
+    else:
+        print('Program is complete')
+        break
+
+# Вариант 2
+while True:
+    try:
+        key = input('Input "+" to continue or "stop" to exit the program: ')
+        if key == 'stop':
+            print('Program exit')
+            break
+        elif key == '+':
+            number1 = input('Input 1st number: ')
+            number2 = input('Input 2nd number: ')
+            result = int(number1) / int(number2)
+            print(number1, '/', number2, '=', result)
+        else:
+            print('incorrect command is entered!')
+    except ValueError:
+        print('Error! Incorrect number is entered')
+        continue
+    except ZeroDivisionError:
+        print('Error! Zero division')
+        continue
+
